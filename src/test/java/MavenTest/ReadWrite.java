7b95015dacbf3c0d63f68e987b34dd8e819e3157
SoorajPillai <soorajpillai1698@gmail.com>
2022-06-22 16:35:11 +0530

    change made

--- src/test/java/MavenTest/ReadWrite.java
+++ src/test/java/MavenTest/ReadWrite.java
@@ -24,7 +24,7 @@ public class ReadWrite {
 	String s=sheet.getRow(row).getCell(col).getStringCellValue();
 	return s;
 	}
-	
+	//s
 	public static void main(String[] args) throws IOException, InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
